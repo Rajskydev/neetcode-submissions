4486ed582d9fa7f42581fb5509ee09cfc6ef6bba
Marek Rajský <34841748+Rajskydev@users.noreply.github.com>
2026-05-04 17:30:06 +0200

    Add: python-private-attribute-and-method - submission-0

---
+++ Python OOP/python-private-attribute-and-method/submission-0.py
@@ -0,0 +1,18 @@
+class PasswordManager:
+    def __init__(self, password):
+        self._password = password
+    
+    def verify_password(self, pwd: str) -> bool:
+        if pwd == self._password:
+            return True
+        else:
+            return False
+    
+
+
+
+
+# Don't modify the code below this line
+my_password = PasswordManager("secret123")
+print(my_password.verify_password("secret123"))  # Should print: True
+print(my_password.verify_password("wrong"))      # Should print: False
